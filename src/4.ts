00e897d59dcd2cf3e0564f1c01fd4bd498230f9d
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2024-01-08 18:12:20 +0200

    almost done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random() * 10;
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: object = new Key();
+  constructor(key: object) {
+    this.key = key;
+  }
+  getKey(): object {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  key: object = new Key();
+  tenants: object[] = [];
+  person: object = new Person(key);
+
+  comeIn(person: object) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract OpenDoor(key: object): object;
+}
+
+class MyHouse extends House {
+  constructor(myKey: object) {
+    super();
+  }
+  openDoor(myKey: object) {
+    if (myKey === this.key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +52,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
